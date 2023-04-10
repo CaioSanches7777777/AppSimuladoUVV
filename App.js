ab0cefa1f070485920a90b79f85f08b497658fa4
CaioSanches7777777 <102621072+CaioSanches7777777@users.noreply.github.com>
2023-04-10 08:26:00 -0300

    Update App.js

--- App.js
+++ App.js
@@ -10,6 +10,12 @@ export default function App() {
       sinopse:'Sinopse de Avatar',
       preco:'R$ 20,00',
       imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg8Rn1TMAdicaFC_bLHYW1oTrcpC53GrxMAQ&usqp=CAU",
+    },
+    {
+      titulo:'Gato de Botas',
+      sinopse:'Sinopse de Gato de Botas',
+      preco:'R$ 18,00',
+      imgUrl:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRLkEdZWNPUZdUItmhnFmJChykum-cYTxG-N6iqXhctW2bu1qQm",
     }
   ]
 
