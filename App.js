c39c749b62be6c80603f4f8642ef35d1bbd1bcb3
CaioSanches7777777 <102621072+CaioSanches7777777@users.noreply.github.com>
2023-04-10 08:41:35 -0300

    fazer lista de filmes em vez de mapa de filmes
    <Filme filme = {filmes[0]}></Filme>
         <Filme filme = {filmes[1]}></Filme>

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Filme from './src/components/filme';
 
-
 export default function App() {
   const filmes = [
     {
